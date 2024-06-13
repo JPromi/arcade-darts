b5ddec0183bdc0c9afe0b36b911307b280c3f390
JPromi <jonasmpr05@gmail.com>
2024-06-13 06:57:20 +0200

    fix env

--- src/app/services/settings.service.ts
+++ src/app/services/settings.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { GameSettings, UserList } from '../dtos/settings';
-import { environment } from '../../environments/environment.development';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
